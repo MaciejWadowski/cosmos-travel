0beddac0814e74cd538602b7b64e1ac92cfe570e
wadziuxxx <macwadowski@tlen.pl>
2019-06-20 13:39:28 +0200

    Initial commit

---
+++ src/main/java/com/example/cosmostravel/CosmosTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.cosmostravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmosTravelApplication.class, args);
+    }
+
+}
